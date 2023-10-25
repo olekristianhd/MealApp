8043e668ae867c3ff91a786ed07019f95fd2ecde
Ole Kristian Dvergsdal <olekd@ntnu.no>
2023-10-25 12:44:46 +0200

    MealApp

---
+++ lib/screens/categories.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
+import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoriesScreen extends StatefulWidget {
+  const CategoriesScreen({
+    Key? key,
+    required this.availableMeals,
+  }) : super(key: key);
+
+  final List<Meal> availableMeals;
+
+  @override
+  _CategoriesScreenState createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> {
+  String? searchQuery;
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = widget.availableMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
+      ),
+    );
+  }
+
+  List<Category> get filteredCategories {
+    if (searchQuery == null || searchQuery!.isEmpty) {
+      return availableCategories;
+    } else {
+      return availableCategories
+          .where((category) =>
+              category.title.toLowerCase().contains(searchQuery!.toLowerCase()))
+          .toList();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            style: const TextStyle(color: Colors.white),
+            onChanged: (value) {
+              setState(() {
+                searchQuery = value;
+              });
+            },
+            decoration: const InputDecoration(
+              labelText: 'Search',
+              labelStyle: TextStyle(color: Colors.white),
+              border: OutlineInputBorder(),
+              prefixIcon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: GridView(
+            padding: const EdgeInsets.all(24),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20,
+            ),
+            children: [
+              for (final category in filteredCategories)
+                CategoryGridItem(
+                  category: category,
+                  onSelectCategory: () {
+                    _selectCategory(context, category);
+                  },
+                )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
